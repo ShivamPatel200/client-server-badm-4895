b931aa138c073d0e0acc333f07b0e8e3b701a25e
ShivamPatel200 <79231571+ShivamPatel200@users.noreply.github.com>
2022-12-11 16:09:55 -0500

    final project, uploaded all files to git hub

---
+++ server_client/client.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python3 client1.py &
+python3 client2.py &
+python3 client3.py &
+python3 client4.py &
+python3 client5.py
---
+++ server_client/client2.py
@@ -0,0 +1,32 @@
+import socket
+import time
+
+HEADER = 64
+PORT = 12800
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "DISCONNECT!"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(f"{msg}")
+    print(f"SERVER: {client.recv(2048).decode(FORMAT)}")
+
+
+for i in range(1, 25):
+    str1 = f"CLIENT2: Data Fragment {i} Sent To Server"
+    send(str1)
+    time.sleep(10)
+
+client.close()
+
+
